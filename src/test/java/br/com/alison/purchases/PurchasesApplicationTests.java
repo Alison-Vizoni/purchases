4ed7417250ff508b2f7ef297fd8dbf318c73b529
Alison-Vizoni <alison.vizoni@gmail.com>
2021-11-16 16:32:50 -0300

    Projeto criado

---
+++ src/test/java/br/com/alison/purchases/PurchasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alison.purchases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
