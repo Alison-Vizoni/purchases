3116c197a83b3ffbe5039c2c81e8205e29c234f4
Alison-Vizoni <alison.vizoni@gmail.com>
2021-11-23 17:54:56 -0300

    adciona novas rotas de crud e validação do client

--- src/main/java/br/com/alison/purchases/resources/ClientResources.java
+++ src/main/java/br/com/alison/purchases/resources/ClientResources.java
@@ -1,13 +1,19 @@
 package br.com.alison.purchases.resources;
 
 import br.com.alison.purchases.domain.Client;
+import br.com.alison.purchases.dto.ClientDTO;
+import br.com.alison.purchases.dto.ClientNewDTO;
 import br.com.alison.purchases.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -20,5 +26,47 @@ public class ClientResources {
     public ResponseEntity<Client> findById(@PathVariable Long id){
         Client client = service.findClientById(id);
         return ResponseEntity.ok().body(client);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ClientDTO>> findAll(){
+        List<Client> categories = service.findAll();
+        List<ClientDTO> categoriesDTO = categories.stream()
+                .map(client -> new ClientDTO(client)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(categoriesDTO);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<ClientDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction){
+        Page<Client> categories = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<ClientDTO> categoriesDTO = categories.map(client -> new ClientDTO(client));
+        return ResponseEntity.ok().body(categoriesDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClientNewDTO clientNewDTO){
+        Client client = service.fromDTO(clientNewDTO);
+        client = service.insert(client);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(client.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody  ClientDTO clientDTO, @PathVariable Long id){
+        Client client = service.fromDTO(clientDTO);
+        client.setId(id);
+        service.update(client);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/com/alison/purchases/service/ClientService.java
+++ src/main/java/br/com/alison/purchases/service/ClientService.java
@@ -1,11 +1,24 @@
 package br.com.alison.purchases.service;
 
+import br.com.alison.purchases.domain.Address;
+import br.com.alison.purchases.domain.City;
 import br.com.alison.purchases.domain.Client;
+import br.com.alison.purchases.domain.enums.ClientType;
+import br.com.alison.purchases.dto.ClientDTO;
+import br.com.alison.purchases.dto.ClientNewDTO;
+import br.com.alison.purchases.repository.AddressRepository;
 import br.com.alison.purchases.repository.ClientRepository;
+import br.com.alison.purchases.service.exceptions.DataIntegrityException;
 import br.com.alison.purchases.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,6 +26,9 @@ public class ClientService {
 
     @Autowired
     private ClientRepository repositry;
+
+    @Autowired
+    private AddressRepository addressRepository;
 
     public Client findClientById(Long id){
         Optional<Client> client = repositry.findById(id);
@@ -22,5 +38,70 @@ public class ClientService {
                 .append(id)
                 .append(", type: ")
                 .append(Client.class.getName()).toString()));
+    }
+
+    @Transactional
+    public Client insert(Client client){
+        client.setId(null);
+        client = repositry.save(client);
+        addressRepository.saveAll(client.getAdresses());
+        return client;
+    }
+
+    public Client update(Client client) {
+        Client newClient = findClientById(client.getId());
+        updateData(newClient, client);
+        return repositry.save(newClient);
+    }
+
+    public void delete(Long id) {
+        findClientById(id);
+        try {
+            repositry.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw  new DataIntegrityException("Cannot delete a client that has relation entities.");
+        }
+    }
+
+    public List<Client> findAll() {
+        return repositry.findAll();
+    }
+
+    public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repositry.findAll(pageRequest);
+    }
+
+    public Client fromDTO(ClientDTO clientDTO){
+        return new Client(clientDTO.getId(), clientDTO.getName(), clientDTO.getEmail(), null, null);
+    }
+
+    public Client fromDTO(ClientNewDTO clientNewDTO){
+        Client client = new Client(null, clientNewDTO.getName(), clientNewDTO.getEmail(),
+                clientNewDTO.getCpfOrCnpj(), ClientType.toEnum(clientNewDTO.getClientType()));
+
+        City city = new City(clientNewDTO.getIdCity(), null, null);
+
+        Address address = new Address(null, clientNewDTO.getStreet(), clientNewDTO.getNumber(),
+                clientNewDTO.getComplement(), clientNewDTO.getDistrict(), clientNewDTO.getPostCode(), client, city);
+
+        client.getAdresses().add(address);
+        client.getPhoneNumbers().add(clientNewDTO.getPhoneNumber());
+
+        if(clientNewDTO.getLandline() != null){
+            client.getPhoneNumbers().add(clientNewDTO.getLandline());
+        }
+
+        if(clientNewDTO.getSecondPhoneNumber() != null){
+            client.getPhoneNumbers().add(clientNewDTO.getSecondPhoneNumber());
+        }
+        return client;
+    }
+
+
+
+    private void updateData(Client newClient, Client client) {
+        newClient.setName(client.getName());
+        newClient.setEmail(client.getEmail());
     }
 }
