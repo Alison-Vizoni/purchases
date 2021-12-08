13955903532026206254e2740d45a72655be5702
Alison-Vizoni <alison.vizoni@gmail.com>
2021-12-08 12:25:13 -0300

    não é mais necessario ler variavel de ambiente da senha do email

--- src/main/java/br/com/alison/purchases/service/SmtpEmailService.java
+++ src/main/java/br/com/alison/purchases/service/SmtpEmailService.java
@@ -1,35 +1,34 @@
 package br.com.alison.purchases.service;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import javax.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService{
 
     @Autowired
-    private JavaMailSenderImpl javaMailSender;
+    private MailSender mailSender;
 
-    private final Dotenv dotenv = Dotenv.configure().load();
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
 
     @Override
     public void sendEmail(SimpleMailMessage simpleMailMessage) {
         LOG.info("Enviando email...");
-        javaMailSender.setPassword(dotenv.get("PASSWORD"));
-        javaMailSender.send(simpleMailMessage);
+        mailSender.send(simpleMailMessage);
         LOG.info("Email enviado.");
     }
 
     @Override
     public void sendHtmlEmail(MimeMessage mimeMessage) {
         LOG.info("Enviando email...");
-        javaMailSender.setPassword(dotenv.get("PASSWORD"));
         javaMailSender.send(mimeMessage);
         LOG.info("Email enviado.");
     }
