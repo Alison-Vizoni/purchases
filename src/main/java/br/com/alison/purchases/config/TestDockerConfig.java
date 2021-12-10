3dd3255fefed225fb4a606b73f872cf899934ed9
Alison-Vizoni <alison.vizoni@gmail.com>
2021-12-10 15:12:02 -0300

    Alterando configurações com profile docker

--- src/main/java/br/com/alison/purchases/config/TestDockerConfig.java
+++ src/main/java/br/com/alison/purchases/config/TestDockerConfig.java
@@ -1,23 +1,10 @@
 package br.com.alison.purchases.config;
 
-import br.com.alison.purchases.service.DBService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.text.ParseException;
-
 @Configuration
-@Profile("test_docker")
+@Profile("integration_test_with_docker")
 public class TestDockerConfig {
 
-    @Autowired
-    private DBService dbService;
-
-    @Bean
-    public boolean instantianteDatabase() throws ParseException {
-        dbService.instantianceTestDatabase();
-        return true;
-    }
 }
